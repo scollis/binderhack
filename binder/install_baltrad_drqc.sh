cc0ad97814d216c5e8bb69c8dbb2cfc430dc4dd4
Daniel Michelson <dbm@baltrad.eu>
2019-11-06 22:59:46 +0000

    ENH: BALTRAD DRQC

--- binder/install_baltrad_drqc.sh
+++ binder/install_baltrad_drqc.sh
@@ -3,6 +3,7 @@ set -x
 
 # Vagrant provision script for installing BALTRAD wrwp from source
 export CONDA_PREFIX=/srv/conda/envs/notebook
+export RAVEROOT=$CONDA_PREFIX
 
 # Install system dependencies, not conda in this case
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$CONDA_PREFIX/hlhdf/lib:$CONDA_PREFIX/rave/lib
